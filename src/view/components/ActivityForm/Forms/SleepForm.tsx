c8389e0d8e28e04a4cf905aad3e2dc0f29187ba8
Gustavo Ribeiro <125397719+ribgu@users.noreply.github.com>
2025-01-17 20:59:37 -0300

    Remove unused variable `sleepDuration` from SleepForm component
    * Remove the `sleepDuration` variable to fix the ESLint error
    * Ensure the form still functions correctly without the `sleepDuration` variable

--- src/view/components/ActivityForm/Forms/SleepForm.tsx
+++ src/view/components/ActivityForm/Forms/SleepForm.tsx
@@ -1,53 +1,39 @@
-import { SetStateAction, useEffect } from 'react'
+import { SetStateAction } from 'react'
 
 type SleepFormProps = {
-  sleepHour: string
-  setSleepHour: React.Dispatch<SetStateAction<string>>
-  wakeUpHour: string
-  setWakeUpHour: React.Dispatch<SetStateAction<string>>
-  sleepDuration: number
-  setSleepDuration: React.Dispatch<SetStateAction<number>>
+  sleepStart: string
+  setSleepStart: React.Dispatch<SetStateAction<string>>
+  sleepEnd: string
+  setSleepEnd: React.Dispatch<SetStateAction<string>>
 }
 
 export function SleepForm(props: SleepFormProps) {
-  const { sleepHour, setSleepHour, wakeUpHour, setWakeUpHour, sleepDuration, setSleepDuration } = props
-
-  useEffect(() => {
-    if (sleepHour && wakeUpHour) {
-      const sleepTime = new Date(sleepHour)
-      const wakeTime = new Date(wakeUpHour)
-      let duration = (wakeTime.getTime() - sleepTime.getTime()) / (1000 * 60 * 60)
-      if (duration < 0) {
-        duration += 24
-      }
-      setSleepDuration(duration)
-    }
-  }, [sleepHour, wakeUpHour, setSleepDuration])
+  const { sleepStart, setSleepStart, sleepEnd, setSleepEnd } = props
 
   return (
     <div className="form-control">
-      <label className="label" htmlFor="sleepHour">
-        <span className="label-text">Hora de Dormir</span>
+      <label className="label" htmlFor="sleepStart">
+        <span className="label-text">Início do Sono</span>
       </label>
       <input
-        id="sleepHour"
-        name="sleepHour"
+        id="sleepStart"
+        name="sleepStart"
         type="datetime-local"
         required
-        value={sleepHour}
-        onChange={(e) => setSleepHour(e.target.value)}
+        value={sleepStart}
+        onChange={(e) => setSleepStart(e.target.value)}
         className="input input-bordered w-full"
       />
-      <label className="label" htmlFor="wakeUpHour">
-        <span className="label-text">Hora de Acordar</span>
+      <label className="label" htmlFor="sleepEnd">
+        <span className="label-text">Fim do Sono</span>
       </label>
       <input
-        id="wakeUpHour"
-        name="wakeUpHour"
+        id="sleepEnd"
+        name="sleepEnd"
         type="datetime-local"
         required
-        value={wakeUpHour}
-        onChange={(e) => setWakeUpHour(e.target.value)}
+        value={sleepEnd}
+        onChange={(e) => setSleepEnd(e.target.value)}
         className="input input-bordered w-full"
       />
     </div>
