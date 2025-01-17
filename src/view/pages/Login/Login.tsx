d0eab888d9ad27bdce446e9db7692bcd2b69febd
ribgu <gustavo.ribeiro@sptech.school>
2025-01-17 10:31:36 -0300

    use next navigation instead next router

--- src/view/pages/Login/Login.tsx
+++ src/view/pages/Login/Login.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 import { useAuth } from '@/contexts/AuthContext'
 
 export function Login() {
