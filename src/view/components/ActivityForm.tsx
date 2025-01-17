3dd2ec462ca9967e5b0ab3a77e9c6840cfeb3f45
Gustavo Ribeiro <125397719+ribgu@users.noreply.github.com>
2025-01-16 22:27:15 -0300

    Add "use client" directive and update imports in ActivityForm component
    * **Imports**
      - Add "use client" directive at the top of the file
      - Replace `next/router` with `next/navigation`

--- src/view/components/ActivityForm.tsx
+++ src/view/components/ActivityForm.tsx
@@ -1,5 +1,7 @@
+"use client"
+
 import React, { useState, useEffect } from 'react'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 import Client from '@/libs/clients/client'
 
 const ActivityForm = () => {
