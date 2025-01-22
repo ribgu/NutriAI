f6ead7d7547801deebc0e9e826efa5a8d91dda51
ribgu <gustavo.ribeiro@sptech.school>
2025-01-21 22:44:19 -0300

    coloca icone de fechar

--- src/view/pages/Water/Water.tsx
+++ src/view/pages/Water/Water.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from 'react'
 import ActivityForm from '@/view/components/ActivityForm/ActivityForm'
 import ActivityList from '@/view/components/ActivityList/ActivityList'
+import { XIcon } from 'lucide-react'
 
 export function Water() {
   const [isModalOpen, setIsModalOpen] = useState(false)
@@ -30,7 +31,7 @@ export function Water() {
                 onClick={() => setIsModalOpen(false)}
                 className="text-gray-500 hover:text-gray-700"
               >
-                ×
+                <XIcon className="h-6 w-6" />
               </button>
             </div>
             <ActivityForm activityType='WATER' />
